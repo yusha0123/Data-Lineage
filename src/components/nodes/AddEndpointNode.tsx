4a2424c6f5e019bc3e37c41eb4ba9ea1aa6e1cbc
Yusha Tahlil <ymmttmy5@gmail.com>
2025-06-18 12:26:56 +0530

    feat: replace SVG icon with AiOutlinePlusCircle for improved visual consistency in AddEndpointNode

--- src/components/nodes/AddEndpointNode.tsx
+++ src/components/nodes/AddEndpointNode.tsx
@@ -1,4 +1,5 @@
 import { Handle, Position } from "@xyflow/react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 interface AddEndPointNodeProps {
   data: {
@@ -12,21 +13,8 @@ interface AddEndPointNodeProps {
 export default function AddEndpointNode({ data }: AddEndPointNodeProps) {
   return (
     <div className="w-60 h-32 bg-green-100 border-2 border-dashed border-green-500 rounded-lg flex flex-col items-center justify-center cursor-pointer hover:bg-green-200">
-      <div className="w-12 h-12 rounded-full flex border-2 border-gray-600 items-center justify-center mb-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-8 w-8 text-gray-600"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
+      <div className="flex items-center justify-center mb-2">
+        <AiOutlinePlusCircle className="size-12 text-gray-500" />
       </div>
       <span className="text-gray-700 font-semibold">{data.label}</span>
 
